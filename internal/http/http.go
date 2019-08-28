8b9f5d7b4e25353ca72030bfb0b1988c2841fb61
Santiago Ignacio Poli <santiago.poli@etermax.com>
2019-08-28 19:00:06 -0300

    Move server startup logic to http package

--- internal/http/http.go
+++ internal/http/http.go
@@ -3,11 +3,12 @@ package http
 import (
 	"net/http"
 
+	"github.com/labstack/echo/middleware"
 	"github.com/labstack/echo"
 	"github.com/santiagopoli/middleman/internal/authorizer"
 )
 
-func AuthorizeRequest(authorizer authorizer.Authorizer) func(echo.Context) error {
+func authorizeRequest(authorizer authorizer.Authorizer) func(echo.Context) error {
 	return func(requestContext echo.Context) error {
 		authzRequestPayload := payloadFrom(requestContext.Request())
 		if authorizer.IsAuthorized(authzRequestPayload) {
@@ -27,4 +28,16 @@ func payloadFrom(request *http.Request) *authorizer.Request {
 
 func addAdditionalHeaders(headers http.Header) http.Header {
 	return headers
+}
+
+func StartServer() {
+	authorizer := authorizer.NewOPAAuthorizer("localhost:8181", "ingress/allow", true)
+	authorizeRequest := authorizeRequest(authorizer)
+	e := echo.New()
+	e.HideBanner = true
+	e.Use(middleware.Logger())
+	e.POST("/authz", authorizeRequest)
+	e.GET("/authz", authorizeRequest)
+
+	e.Logger.Fatal(e.Start(":8080"))
 }
--- main.go
+++ main.go
@@ -2,25 +2,13 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/labstack/echo"
-	"github.com/labstack/echo/middleware"
 	"github.com/mbndr/figlet4go"
-	"github.com/santiagopoli/middleman/internal/authorizer"
 	"github.com/santiagopoli/middleman/internal/http"
 )
 
 func main() {
-	authorizer := authorizer.NewOPAAuthorizer("localhost:8181", "ingress/allow", true)
-	authorizeRequest := http.AuthorizeRequest(authorizer)
-	e := echo.New()
-	e.HideBanner = true
-	e.Use(middleware.Logger())
-	e.POST("/authz", authorizeRequest)
-	e.GET("/authz", authorizeRequest)
-
 	PrintBanner()
-	e.Logger.Fatal(e.Start(":8080"))
+	http.StartServer()
 }
 
 func PrintBanner() {
